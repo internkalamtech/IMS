3c8ca6b4bff6c330e53b6dab589fb0c07dc98025
internkalamtech <intern.kalamtech@gmail.com>
2026-01-16 15:04:19 +0530

    added finance and assessment requirements in JSON format. Also added the powershell script that can be used to add requirements to github

--- scripts/github.ps1
+++ scripts/github.ps1
@@ -17,7 +17,7 @@
 
 # ------------- CONFIG ----------------
 $DRY_RUN = $false             # 🔁 SET TO $false TO EXECUTE FOR REAL
-$ISSUES_FILE = "./requirements/f_issues.json"
+$ISSUES_FILE = "./requirements/finance_req.json"
 $PROJECT_NUMBER = 4          # GitHub Project v2 number
 $OWNER = "internkalamtech"   # user or org
 $REPO = "IMS"                # ⚠️ ADD YOUR REPO NAME HERE
