079cbd718a14b31f8661f9658ce4e9bd46c3a568
sameer <mohammedsamee89@gmail.com>
2026-03-10 07:24:21 +0530

    Initial implementation of class API endpointd

--- backend/app/api/v1/endpoints/classes.py
+++ backend/app/api/v1/endpoints/classes.py
@@ -1,20 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 router = APIRouter()
 
+# temporary storage
 classes = []
 
-# Schema
+# schema
 class ClassCreate(BaseModel):
     name: str
     section: str
 
 
-# Create Class
+# CREATE
 @router.post("/classes")
 def create_class(class_data: ClassCreate):
     new_class = {
+        "id": len(classes) + 1,
         "name": class_data.name,
         "section": class_data.section
     }
@@ -27,7 +29,37 @@ def create_class(class_data: ClassCreate):
     }
 
 
-# Fetch Classes
+# FETCH
 @router.get("/classes")
 def get_classes():
     return classes
+
+
+# UPDATE
+@router.put("/classes/{class_id}")
+def update_class(class_id: int, class_data: ClassCreate):
+
+    for cls in classes:
+        if cls["id"] == class_id:
+            cls["name"] = class_data.name
+            cls["section"] = class_data.section
+            return {
+                "message": "Class updated",
+                "data": cls
+            }
+
+    raise HTTPException(status_code=404, detail="Class not found")
+
+
+# DELETE
+@router.delete("/classes/{class_id}")
+def delete_class(class_id: int):
+
+    for cls in classes:
+        if cls["id"] == class_id:
+            classes.remove(cls)
+            return {
+                "message": "Class deleted"
+            }
+
+    raise HTTPException(status_code=404, detail="Class not found")
