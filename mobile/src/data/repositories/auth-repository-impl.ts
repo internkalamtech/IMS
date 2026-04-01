5f0e4a63ecf60e5fd9e71505f57eed099718febd
far <far@gmail.com>
2026-04-01 19:30:15 +0530

    fix align auth repo with updated user entity

--- mobile/src/data/repositories/auth-repository-impl.ts
+++ mobile/src/data/repositories/auth-repository-impl.ts
@@ -23,6 +23,8 @@ export class AuthRepositoryImpl implements AuthRepository {
                 email: user.email,
                 role: user.role,
                 avatarUrl: user.avatarUrl,
+                marks: 0,
+                attendance: 0,
             };
 
             await StorageService.setItem(TOKEN_STORAGE_KEY, access_token);
